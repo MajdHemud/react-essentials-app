6bcd95cf4a410759b5eb02a8d428e1b21251ed7c
Majd Hemud <majd.sami1996@gmail.com>
2024-01-14 10:06:55 -0500

    adding randomDescription for the react essentials intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandonInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const randomDescription = reactDescriptions[genRandonInt(2)];
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {randomDescription} React concepts you will need for almost any app you
+        are going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
